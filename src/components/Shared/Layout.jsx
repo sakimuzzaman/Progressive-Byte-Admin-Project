c28a04322414db84ba249c526bd644ffe9f0e630
sakimuzzaman <sakimuzzaman104@gmail.com>
2024-11-16 23:58:25 +0600

    1rst commit

--- src/components/Shared/Layout.jsx
+++ src/components/Shared/Layout.jsx
@@ -1,23 +1,4 @@
-// import React from 'react';
-// import {Outlet} from 'react-router-dom';
-// import Sidebar from './Sidebar';
-// import Header from './Header';
 
-// const Layout = () => {
-//     return (
-//         <div className='flex flex-row bg-neutral-100   overflow-hidden'>
-//             <Sidebar />
-//             <div className='flex-1'> 
-//                 <Header />
-//                 <div className='p-4'>{<Outlet/>}</div>
-//             </div>
-
-            
-//         </div>
-//     );
-// };
-
-// export default Layout;
 
 
 
